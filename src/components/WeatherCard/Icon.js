eec84625f71171b7531305606ef7ae6cc1cbc813
Nick Guerra <nickg@maliciousgames.com>
2020-03-25 21:14:38 -0500

    added css in js

---
+++ src/components/WeatherCard/Icon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Icon = (props) => {
+  const Icon = styled.img`
+    width: 40%;
+  `;
+  return <Icon className='icon' src='./img/Mostly Cloudy.png' alt='Weather Icon' />;
+};
+
+export default Icon;
