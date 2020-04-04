68e345b61148c81d52a5a283f94c705f83e5ea3a
Nick Guerra <nickg@maliciousgames.com>
2020-04-04 05:51:36 -0500

    added api call

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import WeatherCard from './components/WeatherCard/component';
 import './App.css';
 
 function App() {
+  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  const apiKey = process.env.REACT_APP_SECRET_KEY;
+
+  const data = async () => {
+    const apiRes = await fetch(
+      'http://api.openweathermap.org/data/2.5/weather?q=London,gb&units=metric' + apiKey
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+
   return (
     <div className='App'>
       <WeatherCard temp={-10} condition='Clear' city='Sydney' country='AU' />
