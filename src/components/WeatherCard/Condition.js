ee4030dd0dae3201e4855e504e0386727365bdea
Nick Guerra <nickg@maliciousgames.com>
2020-08-15 07:30:58 -0500

    removed linting rule on weather engine

--- src/components/WeatherCard/Condition.js
+++ src/components/WeatherCard/Condition.js
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from '@emotion/styled';
 import Reel from 'react-reel';
 
-// const Temp = styled.h1`
-//     font-family: 'Fira Sans', sans-serif;
-//     font-size: 2rem;
-//     font-weight: 200;
-//   `;
+// eslint-disable-next-line
+const Temp = styled.h1`
+    font-family: 'Fira Sans', sans-serif;
+    font-size: 2rem;
+    font-weight: 200;
+  `;
 const State = styled.h3`
     font-family: 'Merriweather', sans-serif;
     font-size: 1.2rem;
